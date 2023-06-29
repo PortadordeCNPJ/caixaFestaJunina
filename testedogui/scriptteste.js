9238584823010cec6bad8f1c254cf2b2d3eb017e
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-06-29 10:51:20 -0300

    ultima att

--- testedogui/scriptteste.js
+++ testedogui/scriptteste.js
@@ -30,15 +30,22 @@ function calcValor(tipoDoce) {
             precoBeij.value = ("R$ " + preco.toFixed(2)).replace(".", ",");
             break;
 
+        case "twoAmores":
+            var inputcontagem = document.querySelector("#ContAmores");
+            var preco = parseInt(inputcontagem.value) * valorDoisAmores;
+            precoAmore.value = ("R$ " + preco.toFixed(2)).replace(".", ",");
+            break;
+
         default:
             break;
     }
 
     var valorTotalinput = document.querySelector("#precototal");
-    var valorTotal = converterStringParaFloat(precoPacoca.value) + converterStringParaFloat(precoCaju.value) + converterStringParaFloat(precoBeij.value);
-    valorTotalinput.value = ("R$ " + valorTotal.toFixed(2)).replace(".", ",")
-
+    var valorTotal = converterStringParaFloat(precoPacoca.value) + converterStringParaFloat(precoCaju.value) + converterStringParaFloat(precoBeij.value) + converterStringParaFloat(precoAmore.value);
+    valorTotalinput.value = ("R$ " + valorTotal.toFixed(2)).replace(".", ",");
 }
+
+
 
 /* Colocar uma função como a de valor logo abaixo, para nao preicsar sempre utilizar o toFixed */
 
@@ -74,39 +81,68 @@ function almentarKg(tipoDoce) {
 
         case "beijinho":
             var inputcontagem1 = document.querySelector("#ContBeijinho");
-            var ContagemCaju = inputcontagem1.value
-            var novovalor1 = parseInt(ContagemCaju) + 1;
-            inputcontagem1.value = novovalor1;
+            var ContagemBeiji = inputcontagem1.value
+            var novovalor2 = parseInt(ContagemBeiji) + 1;
+            inputcontagem1.value = novovalor2;
+            break;
+
+        case "twoAmores":
+            var inputcontagem1 = document.querySelector("#ContAmores");
+            var ContagemAmores = inputcontagem1.value
+            var novovalor3 = parseInt(ContagemAmores) + 1;
+            inputcontagem1.value = novovalor3;
             break;
 
         default:
             break;
 
     }
-    
+
     calcValor(tipoDoce)
 }
 
 function abaixarKg(tipoDoce) {
     console.log(tipoDoce);
-    if (tipoDoce == "pacoca") {
-        var inputcontagem = document.querySelector("#ContPacoca");
-        var ContagemPaco = inputcontagem.value
-        if (ContagemPaco > 0) {
-            var novovalor = parseInt(ContagemPaco) - 1;
-            inputcontagem.value = novovalor;
-        }
-    }
-    else
 
-        if (tipoDoce == "cajuzinho") {
+    switch (tipoDoce) {
+
+        case "pacoca":
+            var inputcontagem = document.querySelector("#ContPacoca");
+            var ContagemPaco = inputcontagem.value
+            if (ContagemPaco > 0) {
+                var novovalor = parseInt(ContagemPaco) - 1;
+                inputcontagem.value = novovalor;
+            }
+            break;
+
+        case "cajuzinho":
             var inputcontagem1 = document.querySelector("#ContCajuzinho");
             var ContagemCaju = inputcontagem1.value
             if (ContagemCaju > 0) {
                 var novovalor1 = parseInt(ContagemCaju) - 1;
                 inputcontagem1.value = novovalor1;
             }
-        }
+            break;
+
+        case "beijinho":
+            var inputcontagem2 = document.querySelector("#ContBeijinho");
+            var ContagemBeiji = inputcontagem2.value
+            if (ContagemBeiji > 0)
+                var novovalor2 = parseInt(ContagemBeiji) - 1;
+            inputcontagem2.value = novovalor2;
+            break;
+
+        case "twoAmores":
+            var inputcontagem3 = document.querySelector("#ContAmores");
+            var ContagemDoisAmo = inputcontagem3.value
+            if (ContagemDoisAmo > 0)
+                var novovalor3 = parseInt(ContagemDoisAmo) - 1;
+            inputcontagem3.value = novovalor3;
+            break;
+
+        default:
+            break;
+    }
     calcValor(tipoDoce)
 }
 
