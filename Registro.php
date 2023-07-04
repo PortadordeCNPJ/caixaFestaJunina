2c26b9388fcb87bfe64ee94c2c6aa4d2570cfee4
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-07-03 23:21:40 -0300

    aa

--- Registro.php
+++ Registro.php
@@ -1,13 +1,37 @@
 <html>
-  <head>
-    <title>Barraca Julina</title>
+
+<head>
+  <title>Barraca Julina</title>
+
   <head>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      integrity="sha384-ggOyR0iXCbMQvX3ipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 
   </head>
-  <body>
 
-  </body>
-  </html>
+<body>
+
+  <form id="form" class="colorful-form" method="post" action="">
+
+    <div class="form-group">
+      <label class="form-label" for="name">Produto:</label>
+      <input required="" placeholder="Insira o produto" class="form-input" id="name" type="text">
+    </div>
+
+    <div class="form-group">
+      <label class="form-label" for="email">Email:</label>
+      <input required="" placeholder="Insira o preço" class="form-input" name="email" id="email" type="email">
+    </div>
+
+    <div class="form-group">
+      <label class="form-label" for="name">Quantidade:</label>
+      <input required="" placeholder="Insira seu CPF" class="form-input" id="CPF" type="text">
+    </div>
+
+  </form>
+
+</body>
+
+</html>
